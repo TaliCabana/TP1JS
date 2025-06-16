d29c13dada1c5a39ac796b432bd888154bd8a5c8
Tali <talicabana@gmail.com>
2025-06-16 17:24:33 -0300

    Resolví el ejercicio 9 del TP 1 de JS

---
+++ JS/ejercicio9.js
@@ -0,0 +1,20 @@
+/* Escribe un programa que pida una frase y escriba las vocales que aparecen
+Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)
+Ejemplo:
+input: Hola mundo
+Output: oauo
+*/
+
+let frase = prompt("Escribí tu frase en MAYUSUCULA y SIN TILDES de hasta 10 caracteres a continuación:")
+let vocales = ""
+
+if length(frase) >= 1 {
+    if { 
+        frase.charAt(0)="A"        
+    }
+    vocales = charAt(0)
+}
+
+
+
+
