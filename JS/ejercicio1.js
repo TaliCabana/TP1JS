aefdfdd4788ce234323b503efd5c5e0315793ae5
Tali <talicabana@gmail.com>
2025-06-15 23:58:07 -0300

    Se resolvió el ejercicio 1 del TP1

---
+++ JS/ejercicio1.js
@@ -0,0 +1,14 @@
+// Escribe un programa de una sola línea que haga que aparezca en la pantalla un alert que diga “un mensaje”.
+
+// Una forma de resolver con más de una linea
+
+/*
+const mensaje = "un mensaje"
+
+console.log(mensaje);
+
+alert(mensaje)
+*/
+
+// Una forma de resolver en una linea
+alert("un mensaje")
